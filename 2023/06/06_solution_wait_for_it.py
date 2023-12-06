831b4a7bd0138748d1355c38c59c38dd2e95ca73
ds7cw <122825406+ds7cw@users.noreply.github.com>
2023-12-06 08:32:08 +0000

    Advent of code 2023, day 6 challenge solution

---
+++ 2023/06/06_solution_wait_for_it.py
@@ -0,0 +1,26 @@
+def margin_of_error(list_collection: list) -> int:
+    result = list_collection[0]
+    for item in list_collection[1:]:
+        result *= item
+    
+    return result
+
+
+with open('06_input.txt', "r") as f:
+    data = f.readlines()
+
+time_list = [int(x) for x in data[0].split(':')[1].strip().split()]
+distance_list = [int(x) for x in data[1].split(':')[1].strip().split()]
+
+counter = 0
+stats = {i: 0 for i in range(1, len(time_list) + 1)}
+
+for seconds, distance in zip(time_list, distance_list):
+    counter +=1 
+
+    for i in range(1, seconds):
+        result = i * (seconds - i)
+        if result > distance:
+            stats[counter]+= 1
+
+print(f'Part 1 Answer: {margin_of_error([val for val in stats.values()])}')
