5102b86d820f93edcb059731b8b55f3a646a8614
ds7cw <122825406+ds7cw@users.noreply.github.com>
2023-12-06 09:01:14 +0000

    Advent of code 2023, day 6 challenge solution Pt 2

--- 2023/06/06_solution_wait_for_it.py
+++ 2023/06/06_solution_wait_for_it.py
@@ -1,3 +1,4 @@
+# Part 1
 def margin_of_error(list_collection: list) -> int:
     result = list_collection[0]
     for item in list_collection[1:]:
@@ -24,3 +25,23 @@ for seconds, distance in zip(time_list, distance_list):
             stats[counter]+= 1
 
 print(f'Part 1 Answer: {margin_of_error([val for val in stats.values()])}')
+
+
+# Part 2
+time = int(''.join([x for x in data[0].split(':')[1].strip().split()]))
+distance = int(''.join([x for x in data[1].split(':')[1].strip().split()]))
+
+first = 0
+last = 0
+
+for i in range(1, time):
+    if i * (time - i) > distance:
+        first = i
+        break
+
+for j in range(time, 0, -1):
+    if j * (time - j) > distance:
+        last = j
+        break
+
+print(f'Part 2 Answer: {last - first + 1}')
