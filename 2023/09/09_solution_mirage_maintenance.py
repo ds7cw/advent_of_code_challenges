565c6bd5a21b12f3ecba600103ab69e9ece15c9e
ds7cw <122825406+ds7cw@users.noreply.github.com>
2023-12-09 10:25:49 +0000

    Advent of code 2023, day 9 challenge solution Pt 2

--- 2023/09/09_solution_mirage_maintenance.py
+++ 2023/09/09_solution_mirage_maintenance.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def check_all_elements_are_zero(some_list: list) -> bool:
     for item in some_list:
         if item != 0:
@@ -8,7 +10,7 @@ def check_all_elements_are_zero(some_list: list) -> bool:
 def extrapolate_next_value(some_list: list) -> None:
     list_length = len(some_list)
     levels.append(some_list)
-    new_list = []
+    new_list = deque()
 
     for i in range(1, list_length):
         current_num = some_list[i] - some_list[i - 1]
@@ -22,19 +24,23 @@ def extrapolate_next_value(some_list: list) -> None:
                 return
             
             current_list, preceeding_list = levels[j], levels[j-1]
+            preceeding_list.appendleft(preceeding_list[0] - (current_list[0]))
             preceeding_list.append(preceeding_list[-1] + current_list[-1])
 
 
 with open('09_input.txt', "r") as f:
     data = f.readlines()
 
-total_sum = 0
+total_sum_pt_1 = 0
+total_sum_pt_2 = 0
 
 for row in data:
     levels = []
-    starting_sequence = [int(x) for x in row.rstrip().split()]
+    starting_sequence = deque(int(x) for x in row.rstrip().split())
 
     extrapolate_next_value(starting_sequence)
-    total_sum += levels[0][-1]
+    total_sum_pt_1 += levels[0][-1]
+    total_sum_pt_2 += levels[0][0]
 
-print(f'Part 1 Answer: {total_sum}')
+print(f'Part 1 Answer: {total_sum_pt_1}')
+print(f'Part 2 Answer: {total_sum_pt_2}')
