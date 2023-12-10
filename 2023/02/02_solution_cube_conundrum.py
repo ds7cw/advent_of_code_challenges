9951a0bec4cc988c8102d83fbad6b58a0caf8a84
ds7cw <122825406+ds7cw@users.noreply.github.com>
2023-12-10 10:12:19 +0000

    Update 02_solution_cube_conundrum.py

--- 2023/02/02_solution_cube_conundrum.py
+++ 2023/02/02_solution_cube_conundrum.py
@@ -19,3 +19,31 @@ for row in data:
     powers_list.append(temp_dict['red'] * temp_dict['green'] * temp_dict['blue'])
 
 print(f'Part 2 Answer: {sum(powers_list)}')
+
+
+# Part 1
+possible_games = []
+
+for row in data:
+    game_data, cubes = row.split(': ')
+    game_id = int(game_data.split()[1])
+ 
+    cubes.rstrip()
+    sets = cubes.split('; ')
+    flag_possible = True
+
+    for cubes_set in sets:
+        for pair in cubes_set.split(', '):
+            cubes_count, colour = pair.split()
+            cubes_count = int(cubes_count)
+
+            if cubes_count > allowed_cubes[colour]:
+                flag_possible = False
+                break
+        if not flag_possible:
+            break
+    
+    else:
+        possible_games.append(game_id)
+
+print(f'Part 1 Answer: {sum(possible_games)}')
